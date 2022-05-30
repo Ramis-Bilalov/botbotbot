2dcb1856e03c68488d5d8cabf1e47606db5f435b
Ramis Bilalov <ramiska81@gmail.com>
2022-05-30 16:27:09 +0300

    2

--- src/main/java/ru/bilalov/mywizard_bot/MyWizardTelegramBot.java
+++ src/main/java/ru/bilalov/mywizard_bot/MyWizardTelegramBot.java
@@ -3,8 +3,12 @@ package ru.bilalov.mywizard_bot;
 import org.telegram.telegrambots.bots.TelegramWebhookBot;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.io.IOException;
+import java.util.List;
 
 public class MyWizardTelegramBot extends TelegramWebhookBot {
     private String webHookPath;
@@ -17,15 +21,43 @@ public class MyWizardTelegramBot extends TelegramWebhookBot {
 
     @Override
     public BotApiMethod onWebhookUpdateReceived(Update update) {
-        if (update.getMessage() != null && update.getMessage().hasText()) {
-            long chat_id = update.getMessage().getChatId();
+        Message message = update.getMessage();
+        if (message != null && message.hasText()) {
             try {
-                execute(new SendMessage(chat_id, "Hi " + update.getMessage().getText()));
-            } catch (TelegramApiException e) {
+                Main main = new Main();
+                main.setNumber(message.getText());
+                List<Article> articleList = main.htmlParse();
+                for (int i = 0; i < articleList.size(); i++) {
+                    if (articleList.get(i).getNum().contains(message.getText())) {
+                        sendMsg(message, articleList.get(i).toString());
+                    }
+                }
+
+            } catch (IOException e) {
                 e.printStackTrace();
             }
+//            long chat_id = update.getMessage().getChatId();
+//            try {
+//                execute(new SendMessage(chat_id, "Hi " + update.getMessage().getText()));
+//
+//            } catch (TelegramApiException e) {
+//                e.printStackTrace();
+//            }
         }
         return null;
+    }
+
+    public void sendMsg(Message message, String text) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(message.getChatId().toString());
+//        sendMessage.setReplyToMessageId(message.getMessageId());
+        sendMessage.setText(text);
+        try {
+            sendApiMethod(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
