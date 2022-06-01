67308e7d31a87768bc86bc29766fbc1903bdc285
Ramis Bilalov <ramiska81@gmail.com>
2022-06-01 15:06:09 +0300

    3

--- src/main/java/ru/bilalov/mywizard_bot/MyWizardBotApplication.java
+++ src/main/java/ru/bilalov/mywizard_bot/MyWizardBotApplication.java
@@ -2,11 +2,15 @@ package ru.bilalov.mywizard_bot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.telegram.telegrambots.ApiContextInitializer;
 
 @SpringBootApplication
+@EnableScheduling
 public class MyWizardBotApplication {
 
     public static void main(String[] args) {
+        ApiContextInitializer.init();
         SpringApplication.run(MyWizardBotApplication.class, args);
     }
 
