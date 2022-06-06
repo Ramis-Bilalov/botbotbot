46c0b56f85981104f5d2d5de1d77c5c2b56e64eb
Ramis Bilalov <ramiska81@gmail.com>
2022-06-06 16:21:19 +0300

    4

--- src/main/java/ru/bilalov/mywizard_bot/Main.java
+++ src/main/java/ru/bilalov/mywizard_bot/Main.java
@@ -28,7 +28,7 @@ public class Main {
         String name = EmojiParser.parseToUnicode(":memo:");
         String money = EmojiParser.parseToUnicode(":abacus:"); // watch
 
-        String url = "https://zakupki.gov.ru/epz/order/extendedsearch/results.html?searchString=" + number + "&morphology=on&search-filter=%D0%94%D0%B0%D1%82%D0%B5+%D1%80%D0%B0%D0%B7%D0%BC%D0%B5%D1%89%D0%B5%D0%BD%D0%B8%D1%8F&pageNumber=1&sortDirection=false&recordsPerPage=_10&showLotsInfoHidden=false&sortBy=UPDATE_DATE&fz44=on&fz223=on&af=on&ca=on&pc=on&pa=on&currencyIdGeneral=-1";
+        String url = "https://zakupki.gov.ru/epz/order/extendedsearch/results.html?searchString=" + number + "&morphology=on&search-filter=%D0%94%D0%B0%D1%82%D0%B5+%D1%80%D0%B0%D0%B7%D0%BC%D0%B5%D1%89%D0%B5%D0%BD%D0%B8%D1%8F&pageNumber=1&sortDirection=false&recordsPerPage=_5&showLotsInfoHidden=false&sortBy=UPDATE_DATE&fz44=on&fz223=on&af=on&ca=on&pc=on&pa=on&currencyIdGeneral=-1";
 
         Document doc = Jsoup.connect(url).get();
 
